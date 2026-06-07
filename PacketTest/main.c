8082a2f9eec1ed091379c0888801ffc24c78c257
Vineeth-0 <vineethm0410@gmail.com>
2026-06-07 16:22:00 -0400

    packet send again

--- PacketTest/main.c
+++ PacketTest/main.c
@@ -1,58 +1,47 @@
 #include <os.h>
 #include <libndls.h>
-#include <stdint.h>
 #include <string.h>
 
-#define SERVICE_STREAM 0x04
+#define SERVICE 0x1234
 
 static nn_ch_t ch;
-static int connected = 0;
+static int ok = 0;
 
-static void navnet_init()
+void init()
 {
     nn_oh_t oh = TI_NN_CreateOperationHandle();
     nn_nh_t node;
 
     if (TI_NN_NodeEnumInit(oh) < 0) return;
     if (TI_NN_NodeEnumNext(oh, &node) < 0) return;
-
     TI_NN_NodeEnumDone(oh);
 
-    if (TI_NN_Connect(node, SERVICE_STREAM, &ch) >= 0) {
-        connected = 1;
-    }
+    ok = (TI_NN_Connect(node, SERVICE, &ch) >= 0);
 
     TI_NN_DestroyOperationHandle(oh);
 }
 
-static void send_msg(const char *msg)
+void send(const char *msg)
 {
-    if (!connected)
-        navnet_init();
-
-    if (!connected)
-        return;
-
-    TI_NN_Write(ch, (void *)msg, strlen(msg));
+    if (!ok) return;
+    TI_NN_Write(ch, (void*)msg, strlen(msg));
 }
 
-int main(void)
+int main()
 {
     assert_ndless_rev(893);
 
-    printf("NavNet stream running...\n");
-
-    navnet_init();
+    init();
 
     int i = 0;
     char buf[64];
 
     while (1)
     {
-        sprintf(buf, "MSG_%d", i++);
-        send_msg(buf);
+        sprintf(buf, "hello %d", i++);
+        send(buf);
 
-        msleep(1000);  // IMPORTANT: prevents CX II crashes
+        msleep(1000);
     }
 
     return 0;
